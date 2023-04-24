63a8cea55b8472e51a2d399387e59851b5d0abe6
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-24 18:58:59 -0300

    feat: Criado o servico de nota fiscal

---
+++ src/app/nota-fiscal/service/nota-fiscal.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NotaFiscal } from '../model/nota-fiscal';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotaFiscalService {
+  constructor(private readonly _httpClient: HttpClient) {}
+
+  // API endpoint/
+  apiURL = 'http://localhost:3000/notas';
+
+  loadNota(uuid: string) {
+    return this._httpClient.get(this.apiURL + '/' + uuid);
+  }
+
+  loadNotas() {
+    return this._httpClient.get(this.apiURL);
+  }
+
+  createNota(nota: NotaFiscal) {
+    return this._httpClient.post(this.apiURL, nota).subscribe();
+  }
+
+  updateNota(uuid: string, nota: NotaFiscal): Observable<NotaFiscal> {
+    return this._httpClient.patch<NotaFiscal>(this.apiURL + uuid, nota);
+  }
+
+  deleteNota(uuid: string): Observable<any> {
+    return this._httpClient.delete(this.apiURL + uuid);
+  }
+}
