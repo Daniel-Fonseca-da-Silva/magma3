10498901eff05c39264f0ac3e4cefcde3375ec55
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-27 12:43:40 -0300

    feat: Definidas as rotas para header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NotaFiscalModule } from './nota-fiscal/nota-fiscal.module';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/view-model/header.component';
+import { Routes } from '@angular/router';
+import { NotaFiscalComponent } from './nota-fiscal/view-model/nota-fiscal.component';
+import { HeaderModule } from './header/header.module';
+
+const appRoutes: Routes = [
+  // { path: '', component: HomeComponent },
+  { path: 'notas', component: NotaFiscalComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +24,7 @@ import { HttpClientModule } from '@angular/common/http';
     AppRoutingModule,
     FormsModule,
     NotaFiscalModule,
+    HeaderModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
