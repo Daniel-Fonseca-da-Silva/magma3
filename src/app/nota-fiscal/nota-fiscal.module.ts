2e26c06a4d857c4826ba1e1fc55a4d905317c057
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-27 13:59:42 -0300

    feat: Adicionadas algumas configuracoes

--- src/app/nota-fiscal/nota-fiscal.module.ts
+++ src/app/nota-fiscal/nota-fiscal.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { NotaFiscalComponent } from './view-model/nota-fiscal.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderModule } from '../header/header.module';
 
 @NgModule({
   declarations: [NotaFiscalComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    HeaderModule,
+  ],
   exports: [NotaFiscalComponent],
 })
 export class NotaFiscalModule {}
