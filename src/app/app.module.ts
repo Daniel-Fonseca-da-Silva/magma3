f8f77ee6a6a2f60e28d67b07dc73a16a13b00eb7
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-27 13:42:50 -0300

    Feat: Adicionada imagens e algumas configuracoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { NotaFiscalModule } from './nota-fiscal/nota-fiscal.module';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './header/view-model/header.component';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NotaFiscalComponent } from './nota-fiscal/view-model/nota-fiscal.component';
 import { HeaderModule } from './header/header.module';
+import { MainComponent } from './main/view-model/main.component';
 
 const appRoutes: Routes = [
-  // { path: '', component: HomeComponent },
+  { path: '', component: MainComponent },
   { path: 'notas', component: NotaFiscalComponent },
 ];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
@@ -25,6 +24,7 @@ const appRoutes: Routes = [
     FormsModule,
     NotaFiscalModule,
     HeaderModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent],
