abf0f7efc5d8030f8a998ec6c09eb4f3f3779341
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-27 13:42:05 -0300

    feat: Criado o componente principal

---
+++ src/app/main/view-model/main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: '../view/main.component.html',
+  styleUrls: ['../view/main.component.css'],
+})
+export class MainComponent {
+  constructor(private readonly router: Router) {}
+  btnClick = () => this.router.navigateByUrl('/notas');
+}
