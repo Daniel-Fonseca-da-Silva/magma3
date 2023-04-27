90fd3378e3133fee823eeb21bf487f80ae615dac
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2023-04-27 12:26:46 -0300

    feat: Configurada as funcionalidades para o form

--- src/app/nota-fiscal/nota-fiscal.module.ts
+++ src/app/nota-fiscal/nota-fiscal.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewModelComponent } from './view-model/nota-fiscal.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [ViewModelComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   exports: [ViewModelComponent],
 })
 export class NotaFiscalModule {}
--- src/app/nota-fiscal/view-model/nota-fiscal.component.ts
+++ src/app/nota-fiscal/view-model/nota-fiscal.component.ts
@@ -1,18 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NotaFiscal } from '../model/nota-fiscal';
 import { NotaFiscalService } from '../service/nota-fiscal.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nota-form',
   templateUrl: '../view/nota-fiscal.html',
   styleUrls: ['../view/nota-fiscal.css'],
 })
-export class ViewModelComponent {
-  constructor(private notaService: NotaFiscalService) {}
-  nota!: NotaFiscal;
+export class ViewModelComponent implements OnInit {
+  constructor(
+    private notaService: NotaFiscalService,
+    private readonly fb: FormBuilder,
+    private readonly router: Router
+  ) {}
+  notasForm!: FormGroup;
+  notas$!: NotaFiscal;
 
-  save() {
-    console.log('nota: ' + this.nota);
+  ngOnInit(): void {
+    this.notasForm = this.initForm();
+  }
+
+  changeRouter(rota: string) {
+    this.router.navigateByUrl(rota);
+  }
+
+  clearText() {
+    this.notasForm.reset();
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      nome: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      preco: ['', [Validators.required]],
+      data: ['', [Validators.required]],
+    });
+  }
+
+  submitForm() {
+    this.notas$ = {
+      nome: this.notasForm.value.nome,
+      email: this.notasForm.value.email,
+      preco: this.notasForm.value.preco,
+      data: this.notasForm.value.data,
+    };
+
+    this.notaService.createNota(this.notas$);
+    // this.router.navigate(['/?']);
   }
 
   carregarNota(uuid: string) {
@@ -21,10 +57,6 @@ export class ViewModelComponent {
 
   carregarNotas() {
     this.notaService.loadNotas().subscribe();
-  }
-
-  CriarNota(nota: NotaFiscal) {
-    this.notaService.createNota(nota);
   }
 
   atualizarNota(uuid: string, nota: NotaFiscal) {
